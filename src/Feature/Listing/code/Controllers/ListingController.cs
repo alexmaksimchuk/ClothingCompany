cc60b4eea79eb363d593f9e2faed8f1deca3b2c4
Alex Maksimchuk <alexmaksimchuk1999@gmail.com>
2021-07-07 00:30:19 +0300

    refactor solution according to Helix, configure Unicorn

---
+++ src/Feature/Listing/code/Controllers/ListingController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClothingCompany.Feature.Listing.Models;
+using Sitecore.Links;
+using Sitecore.Mvc.Presentation;
+
+namespace ClothingCompany.Feature.Listing.Controllers
+{
+    public class ListingController : Controller
+    {
+        // GET: Listing
+        public ActionResult Index()
+        {
+            var dataSourceId = RenderingContext.CurrentOrNull.Rendering.DataSource;
+            var pageList = new Models.Listing();
+
+            if (dataSourceId != null)
+            {
+                var dataSource = Sitecore.Context.Database.GetItem(dataSourceId);
+
+                if (dataSource != null)
+                {
+                    var listOfArticles = dataSource.Children;
+
+                    pageList.Posts = listOfArticles.Select(item => new NavItem
+                    {
+                        Item = item,
+                        Url = LinkManager.GetItemUrl(item)
+                    }).ToList();
+                }
+            }
+
+            return View(pageList);
+        }
+    }
+}
---
+++ src/Feature/Listing/code/Models/Listing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Data.Items;
+
+namespace ClothingCompany.Feature.Listing.Models
+{
+    public class Listing
+    {
+        public Item ParentItem { get; set; }
+
+        public IEnumerable<NavItem> Posts { get; set; } = Enumerable.Empty<NavItem>();
+    }
+}
