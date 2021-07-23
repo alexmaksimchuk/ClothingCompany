b009a042b03c8476d7c7c251567c0385b0a8ab94
Alex Maksimchuk <alexmaksimchuk1999@gmail.com>
2021-07-23 21:00:41 +0300

    create compatible rendering for carousel component

--- src/Feature/Carousel/code/Controllers/CarouselController.cs
+++ src/Feature/Carousel/code/Controllers/CarouselController.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using ClothingCompany.Feature.Carousel.Models;
 using Sitecore.Collections;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Data.Query;
 using Sitecore.Mvc.Presentation;
 
 namespace ClothingCompany.Feature.Carousel.Controllers
@@ -23,6 +27,20 @@ namespace ClothingCompany.Feature.Carousel.Controllers
             carouselDataViewModel.SlideItems = slideItems;
 
             return PartialView(carouselDataViewModel);
+        }
+
+        public ActionResult CompatibleRendering()
+        {
+            Item slide = null;
+
+            var dataSourceItem = Sitecore.Context.Database.GetItem("/sitecore/content/Home/Metadata/Carousel/Slides");
+
+            if (dataSourceItem != null)
+            {
+                slide = dataSourceItem.Children.FirstOrDefault(item => item.DescendsFrom(new ID("{0E00829D-7F26-412E-8E7D-629761A71191}")));
+            }
+
+            return PartialView(slide);
         }
     }
 }
